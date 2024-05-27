ecc1af30c56b0ae0ea60f53b275045001dd88703
nicsoellner <nicolas.soellner@uni-bayreuth.de>
2024-05-27 20:42:33 +0200

    Implemented 4.2.1

--- business/src/test/java/de/unibayreuth/se/teaching/list/business/impl/InsertIntoListStepDefs.java
+++ business/src/test/java/de/unibayreuth/se/teaching/list/business/impl/InsertIntoListStepDefs.java
@@ -1,76 +1,24 @@
 package de.unibayreuth.se.teaching.list.business.impl;
 
-import io.cucumber.java.en.Given;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
 
 public class InsertIntoListStepDefs {
     private DoublyLinkedList list;
-    private double value;
+    
 
-
-    @When("I choose to insert an element with value {double}")
-    public void i_choose_to_insert_an_element_with_value(Double double1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("the list should contain {int} element")
-    public void the_list_should_contain_element(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("the list should contain that element")
-    public void the_list_should_contain_that_element() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-
-    /**
     @Before
     public void initialization() {
-        this.list = new DoublyLinkedList();
+        list = new DoublyLinkedList();
     }
 
-    @Given("an empty list")
-    public void an_empty_list() {
-        Assertions.assertTrue(this.list.isEmpty());
-    }
-
-    @When("I choose to insert an element with value {double}")
-    public void iChooseToInsertAnElementWithValue(double arg0) {
-            list.append(new Value(arg0));
-    }
-
-    @Then("the list should contain {int} element")
-    public void theListShouldContainElement(int arg0) {
-        Assertions.assertEquals(1, this.list.size());
+    @Then("it should be sorted")
+    public void it_should_be_sorted() {
+        double[] array = list.asArray();
+        for(int i = 0; i < list.getLength() - 1; i++) {
+            Assertions.assertTrue(array[i] <= array[i + 1]);
+        }
 
     }
-
-    @And("the list should contain that element")
-    public void theListShouldContainThatElement() {
-        Assertions.assertTrue(this.list.contains(value)));
-    }
-
-    @io.cucumber.java.en.When("I append an element with value {double}")
-    public void iAppendAnElementWithValue(int arg0, int arg1) {
-    }
-
-    @io.cucumber.java.en.Then("the list should contain {int} elements and it should be sorted")
-    public void theListShouldContainElementsAndItShouldBeSorted(int arg0) {
-    }
-
-    @io.cucumber.java.en.And("the list should contain the elements in the following order:")
-    public void theListShouldContainTheElementsInTheFollowingOrder() {
-    }
-
-    @io.cucumber.java.en.Given("a list with the following elements:")
-    public void aListWithTheFollowingElements() {
-    }
-
-    @io.cucumber.java.en.When("I insert an element with value {double}")
-    public void iInsertAnElementWithValue(int arg0, int arg1) {
-    }
-    **/
 }
--- business/src/test/resources/de/unibayreuth/se/teaching/list/business/impl/insert-into-list.feature
+++ business/src/test/resources/de/unibayreuth/se/teaching/list/business/impl/insert-into-list.feature
@@ -3,7 +3,7 @@ Feature: InsertIntoListTests
 
   Scenario: Insert one element to the list
     Given an empty list
-    When I choose to insert an element with value 0.1
+    When I append an element with value 0.1
     Then the list should contain 1 element
     And the list should contain that element
 
@@ -11,18 +11,14 @@ Feature: InsertIntoListTests
     Given an empty list
     When I append an element with value 0.3
     And I append an element with value 0.2
-    Then the list should contain 2 elements and it should be sorted
-    And the list should contain the elements in the following order:
-      | 0.2 |
-      | 0.3 |
+    Then the list should contain 2 elements
+    And it should be sorted
+
 
   Scenario: Insert an element in the middle of the list
-    Given a list with the following elements:
+    Given I have elements with the following values in my list:
       | 0.1 |
       | 0.3 |
-    When I insert an element with value 0.2
-    Then the list should contain 3 elements and it should be sorted
-    And the list should contain the elements in the following order:
-      | 0.1 |
-      | 0.2 |
-      | 0.3 |
+    When I append an element with value 0.2
+    Then the list should contain 3 elements
+    And it should be sorted
